a05239913b49edb1565a54d94afd3de4a6fc1568
Hameedat Oyewopo <hameedattimileyinoyewopo@gmail.com>
2024-12-12 20:46:26 +0100

    deleted loader page

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,10 @@ import Header from "./component/Header";
 import Meals from "./component/Meals";
 import { CartContextProvider } from "./store/CartContext";
 import UserProgressContextProvider from "./store/UserProgressContext";
-import LoadingSpinner from "./component/LoadingPage";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-        setLoading(false); 
-    }, 4000);
-
-    return () => clearTimeout(timer); 
-}, []);
-
   return (
     <>
-    {loading && <LoadingSpinner />} 
-    {!loading && (
     <UserProgressContextProvider>
     <CartContextProvider>
     <Header/>
@@ -30,7 +17,7 @@ function App() {
     <Checkout/>
     </CartContextProvider>
     </UserProgressContextProvider>
-    )}
+
     </>
   );
 }
